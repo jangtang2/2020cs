e651d6e5687bd57fb7762fd25301f99a42d617d5
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-12-13 22:24:56 +0900

    Create class step 2
    -import settings
    -create scanner
    -create char type cube

---
+++ CS/src/codesquad/step2.java
@@ -0,0 +1,16 @@
+package codesquad;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class step2 { 
+    static Scanner scanner = new Scanner(System.in);
+    static char[][] cube = new char[3][3];
+
+    public static void main(String[] args) {
+    	
+    }
+ 
+} 

d5df591941ea2a3f5f1d7fac62ac97a60de0251a
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-12-13 22:26:25 +0900

    Fill cube
    -create list
    -fill cube with chars

--- CS/src/codesquad/step2.java
+++ CS/src/codesquad/step2.java
@@ -10,7 +10,10 @@ public class step2 {
     static char[][] cube = new char[3][3];
 
     public static void main(String[] args) {
-    	
+    	List<String> list = new ArrayList<String>();
+		cube[0] = new char[]{'R', 'R', 'W'};
+        cube[1] = new char[]{'G', 'C', 'W'};
+        cube[2] = new char[]{'G', 'B', 'B'};
     }
  
 } 

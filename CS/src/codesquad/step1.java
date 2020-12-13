fd3c2c2f2963257c3c6c9389d9549413a0a520ae
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-12-13 22:14:21 +0900

    Make step 1 class
    -import settings
    -variable declaration

---
+++ CS/src/codesquad/step1.java
@@ -0,0 +1,21 @@
+package codesquad;
+
+import java.util.Scanner;
+
+
+public class step1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String word;
+		String w_arr[];
+		int num;
+		char LR;
+		boolean flag = true;
+		int len;
+		String result[];
+		
+		
+	}
+}
